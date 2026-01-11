dcb526b193e57fba269ea974288b1aa89438130c
mflehaeior243 <hriojaafabrici@gmail.com>
2026-01-11 10:30:54 -0400

    Entidad produccion con controller y migration

--- Data/simpleContext.cs
+++ Data/simpleContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using simple.Entidades;
+using simple.Models;
 
 namespace simple.Data
 {
@@ -16,6 +17,6 @@ namespace simple.Data
 
         public DbSet<Articulo> Articulo { get; set; } = default!;
         public DbSet<ContabilidadFactura> ContabilidadFacturas { get; set; } = default!;
-        
+        public DbSet<ProduccionContenido> ProduccionContenidos {get; set;} = default!;
     }
 }
