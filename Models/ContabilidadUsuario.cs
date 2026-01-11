bf85444a043a50ffca6a567aa7a9e66b5a9cfb74
Miguel Angel Colque Calizaya <miguelcolq4@gmail.com>
2026-01-11 17:54:34 -0400

    Mi trabajo: Agrega ContabilidadUsuario

---
+++ Models/ContabilidadUsuario.cs
@@ -0,0 +1,32 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EjemploNubeArticulo.Models
+{
+    public class ContabilidadUsuario
+    {
+        [Key]
+        public int CON_Id { get; set; }
+
+    
+        public string CON_Nombre { get; set; } = "";
+
+        public string CON_Email { get; set; } = "";
+
+
+        public string CON_Rol { get; set; } = "Usuario";
+
+
+        public DateTime CON_FechaRegistro { get; set; }
+
+        
+        public string CON_Departamento { get; set; } = "Contabilidad";
+
+        public string CON_Telefono { get; set; } = "";
+
+
+        public bool CON_Activo { get; set; } = true;
+
+        public decimal CON_Salario { get; set; }
+    }
+}
